8d4d469f06c6b483a09bac4b73218f35dbf7960f
Dom <dom@nonacreative.com>
2019-04-20 13:48:58 +0200

    merge

---
+++ client/src/components/page4.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const page4 = () => {
+  return (
+    <section>
+      <header>Books delivered?</header>
+      <main>
+        <div className="ui card">
+          <img
+            src="https://www.allposters.com/images/pic/54/039_22741~Arnold-Schwarzenegger-Posters.jpg"
+            className="ui image"
+            alt="blah"
+          />
+          <div className="content">
+            <div className="header">Matthew</div>
+            <div className="description">
+              Matthew is a musician living in Nashville.
+            </div>
+          </div>
+        </div>
+      </main>
+    </section>
+  )
+}
+
+export default page4
